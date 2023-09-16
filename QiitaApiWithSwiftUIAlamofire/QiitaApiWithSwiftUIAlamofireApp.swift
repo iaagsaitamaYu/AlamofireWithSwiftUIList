eaf58eade045692ab5894471aed8deb62deab658
iaagsaitamaYu <iaagsaitama@gmail.com>
2023-09-16 21:48:58 +0900

    Initial Commit

---
+++ QiitaApiWithSwiftUIAlamofire/QiitaApiWithSwiftUIAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaApiWithSwiftUIAlamofireApp.swift
+//  QiitaApiWithSwiftUIAlamofire
+//
+//  Created by 箭内雄 on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct QiitaApiWithSwiftUIAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
